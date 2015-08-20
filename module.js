f51f9e914ecb2247bbd65e7cf4c8d27c612aa27e
Rostyslav <rostowner@gmail.com>
2015-08-20 11:34:13 +0300

    cool

--- module.js
+++ module.js
@@ -1,32 +1,32 @@
 "use strict";
 
 var Person = function(obj) {
-	var _sexEnum = {
+	var sexEnum = {
 			male: 1,
 			female: 0
 		},
 		_name = obj.name || "",
 		_age = obj.age || 25,
-		_sex = obj.sex || _sexEnum.male;
+		_sex = obj.sex || sexEnum.male;
 
 		return {
 				setName: function() {
 
 				},
 				getName: function() {
-
+					return _name;
 				},
 				setAge: function() {
 
 				},
 				getAge: function () {
-					// body...
+					return _age;
 				},
 				setSex: function () {
 					// body...
 				},
 				getSex: function () {
-					// body...
+					return _sex? "male": "female";
 				},
 				say: function () {
 					console.log("Hi "+_name);
@@ -34,8 +34,37 @@ var Person = function(obj) {
 		};
 };
 
-var rost = new Person({name: "Rost", age: 27, sex: 1});
-var ivan = new Person({name: "Ivan", age: 25, sex: 1});
+var Humans = (function(){
+	
+	var self = {},
+		sexEnum = {
+				male: 1,
+				female: 0
+			},
+		_list = [],
+		_addHuman = function (obj) {
+			_list.push(new Person({name: obj.name, age: obj.age, sex: obj.sex}));
+		};
+	
+	self.init = function () {
+		_addHuman({name: "Rost", age: 27, sex: sexEnum.male});
+		_addHuman({name: "Ivan", age: 25, sex: sexEnum.male});
+	}();
 
-rost.say();
-ivan.say();
+	self.getAllHumans = function () {
+		console.log([].concat(_list.map(function (x) {
+			return x.getName() + " " + x.getAge() + " " + x.getSex();
+		})));
+	};
+
+	self.burnNewHuman = function (data) {
+		if (!data) {
+			return;
+		}
+		_addHuman(data);
+	};
+
+	return self;
+}());
+
+Humans.getAllHumans();
